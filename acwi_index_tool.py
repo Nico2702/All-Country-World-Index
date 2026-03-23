fd3653ca78c6e6e98ff8d39599d63ed44556d999
Nico <hamann.nico@gmail.com>
2026-03-23 11:27:01 +0100

    Update acwi_index_tool.py

--- acwi_index_tool.py
+++ acwi_index_tool.py
@@ -290,18 +290,24 @@ with st.sidebar:
         except: return 0
 
     st.markdown("**DM ADTV**")
-    _dm1, _dm2 = st.columns(2)
-    with _dm1:
-        dm_min_adtv_6m  = parse_adtv(st.text_input("6M (Threshold)", value="0", key="dm_6m"))
-    with _dm2:
-        dm_min_adtv_12m = parse_adtv(st.text_input("12M (Threshold)", value="0", key="dm_12m"))
+    _dma, _dmb = st.columns([1, 3])
+    with _dma: dm_use_6m  = st.checkbox("6M",  value=False, key="dm_use_6m")
+    with _dmb: dm_min_adtv_6m  = parse_adtv(st.text_input("6M (Threshold)",  value="0", key="dm_6m",  disabled=not dm_use_6m))
+    _dmc, _dmd = st.columns([1, 3])
+    with _dmc: dm_use_12m = st.checkbox("12M", value=False, key="dm_use_12m")
+    with _dmd: dm_min_adtv_12m = parse_adtv(st.text_input("12M (Threshold)", value="0", key="dm_12m", disabled=not dm_use_12m))
+    if not dm_use_6m:  dm_min_adtv_6m  = 0
+    if not dm_use_12m: dm_min_adtv_12m = 0
 
     st.markdown("**EM ADTV**")
-    _em1, _em2 = st.columns(2)
-    with _em1:
-        em_min_adtv_6m  = parse_adtv(st.text_input("6M (Threshold)", value="0", key="em_6m"))
-    with _em2:
-        em_min_adtv_12m = parse_adtv(st.text_input("12M (Threshold)", value="0", key="em_12m"))
+    _ema, _emb = st.columns([1, 3])
+    with _ema: em_use_6m  = st.checkbox("6M",  value=False, key="em_use_6m")
+    with _emb: em_min_adtv_6m  = parse_adtv(st.text_input("6M (Threshold)",  value="0", key="em_6m",  disabled=not em_use_6m))
+    _emc, _emd = st.columns([1, 3])
+    with _emc: em_use_12m = st.checkbox("12M", value=False, key="em_use_12m")
+    with _emd: em_min_adtv_12m = parse_adtv(st.text_input("12M (Threshold)", value="0", key="em_12m", disabled=not em_use_12m))
+    if not em_use_6m:  em_min_adtv_6m  = 0
+    if not em_use_12m: em_min_adtv_12m = 0
 
     st.markdown("---")
     st.markdown("<div style='color:#8892b0;font-size:11px;'>NaroIX Index Construction Tool<br/>© 2025 NaroIX</div>", unsafe_allow_html=True)
